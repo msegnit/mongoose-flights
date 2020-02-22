44ec8802a1602eb946bc023bb09b4a751c9a33de
msegnit <melsegnit@gmail.com>
2020-02-22 16:29:21 -0600

    first commit

---
+++ models/flight.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const flightSchema = new Schema({
+    airline: {
+        type: String,
+        enum:['American', 'Southwest', 'United']
+},
+ flightNo: {
+     type: Number,
+     require: true,
+     min: 10,
+     max: 9999
+  },
+  departs: { 
+    type: Date,
+    default: function(){
+      return new Date().getFullYear()+1;
+    }
+    }
+})
